7b3870a1290b7fa8b2588fc3cd6c5485fbbfa91f
Volodohus <volodohus@gmail.com>
2023-10-29 16:45:42 +0300

    проверь мигалку

--- Table.py
+++ Table.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 bd =  sqlite3.connect('Basuka.db')
-
 kluc = bd.cursor()
 
 kluc.execute('''CREATE TABLE IF NOT EXISTS Tusa(
@@ -9,3 +8,11 @@ kluc.execute('''CREATE TABLE IF NOT EXISTS Tusa(
 	Name text,
 	Role text);''')
 kluc.execute('''INSERT into Tusa(Name,Role) VALUES ('Pavel', 'leading');''')
+kluc.execute('''INSERT into Tusa(Name,Role) VALUES ('Egor', 'player');''')
+kluc.execute('''INSERT into Tusa(Name,Role) VALUES ('Anna', 'player');''')
+kluc.execute('''INSERT into Tusa(Name,Role) VALUES ('Alina', 'Assistant');''')
+
+kluc.execute('''select* from Tusa;''')
+bd.commit()
+s = kluc.fetchall()
+print(s)
